0157e3af8ae832d82276a839e1d721b24e1f595c
Sierra McCahon <smitche4@mail.umw.edu>
2018-07-25 01:02:00 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
-class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+lass Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -10,4 +12,11 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+    #or GENRES = @genre
+  end
+
 end
+ 
